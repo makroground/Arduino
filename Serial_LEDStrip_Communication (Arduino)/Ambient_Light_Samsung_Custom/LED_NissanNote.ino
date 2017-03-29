2d171715c581fb557f5f0f66d4f08582b39b4b0a
makroground <r.bressler@makroground.de>
2017-03-29 22:07:50 +0200

    Added Time Relay Function
    Trigger Stripes off, when IGN 12V goes off

--- Serial_LEDStrip_Communication (Arduino)/Ambient_Light_Samsung_Custom/LED_NissanNote.ino
+++ Serial_LEDStrip_Communication (Arduino)/Ambient_Light_Samsung_Custom/LED_NissanNote.ino
@@ -10,8 +10,8 @@
 #define LED_maxCount1 12 // other: 5x 1m Strips a 30 LED's 
 #define LED_maxCount2 6
 #define LED_maxCount3 5
-#define LED_maxCount4 10
-#define LED_maxCount5 10
+#define LED_maxCount4 6
+#define LED_maxCount5 6
 #define LED_maxCount6 15
 
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(LED_maxCount1, PIN_STRIP1, NEO_GRB + NEO_KHZ800);
@@ -21,8 +21,23 @@ Adafruit_NeoPixel strip4 = Adafruit_NeoPixel(LED_maxCount4, PIN_STRIP4, NEO_GRB 
 Adafruit_NeoPixel strip5 = Adafruit_NeoPixel(LED_maxCount5, PIN_STRIP5, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip6 = Adafruit_NeoPixel(LED_maxCount6, PIN_STRIP6, NEO_GRB + NEO_KHZ800);
 
+int relayBATT = 2;
+int relayIGN = 4;
+
+const long interval = 30000;
+unsigned long previousMillis = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(PIN_STRIP1, OUTPUT);
+  pinMode(PIN_STRIP2, OUTPUT);
+  pinMode(PIN_STRIP3, OUTPUT);
+  pinMode(PIN_STRIP4, OUTPUT);
+  pinMode(PIN_STRIP5, OUTPUT);
+  pinMode(PIN_STRIP6, OUTPUT);
+  pinMode(relayBATT, OUTPUT);
+  pinMode(relayIGN, INPUT);
+  
   Serial.begin(115200);
   strip1.begin();
   strip1.setBrightness(200);
@@ -36,41 +51,57 @@ void setup() {
   strip5.setBrightness(200);
   strip6.begin();
   strip6.setBrightness(200);
-  
+
+  digitalWrite(relayBATT, LOW);
 }
 
 void loop() {
-  // Aktiviere alle Pixel in einer Farbe
-  for (uint16_t i = 0; i < strip1.numPixels(); i++)
-  {
-      strip1.setPixelColor(i, strip1.Color(150, 210, 255));
+  unsigned long currentMillis = millis();
+
+  //Wiederhole loop() alle 30000 msec (Interval)
+  if (currentMillis - previousMillis >= interval) {
+    // Speichere letzten Invervalzeitpunkt
+    previousMillis = currentMillis;
+
+    // Aktiviere alle Pixel in einer Farbe
+    for (uint16_t i = 0; i < strip1.numPixels(); i++)
+    {
+        strip1.setPixelColor(i, strip1.Color(150, 210, 255));
+    }
+    strip1.show();
+    for (uint16_t i = 0; i < strip2.numPixels(); i++)
+    {
+        strip2.setPixelColor(i, strip2.Color(150, 210, 255));
+    }
+    strip2.show();
+    for (uint16_t i = 0; i < strip3.numPixels(); i++)
+    {
+        strip3.setPixelColor(i, strip3.Color(150, 210, 255));
+    }
+    strip3.show();
+    for (uint16_t i = 0; i < strip4.numPixels(); i++)
+    {
+        strip4.setPixelColor(i, strip4.Color(150, 210, 255));
+    }
+    strip4.show();
+    for (uint16_t i = 0; i < strip5.numPixels(); i++)
+    {
+        strip5.setPixelColor(i, strip5.Color(150, 210, 255));
+    }
+    strip5.show();
+    for (uint16_t i = 0; i < strip6.numPixels(); i++)
+    {
+        strip6.setPixelColor(i, strip6.Color(150, 210, 255));
+    }
+    strip6.show();
   }
-  strip1.show();
-  for (uint16_t i = 0; i < strip2.numPixels(); i++)
-  {
-      strip2.setPixelColor(i, strip2.Color(150, 210, 255));
+
+  // Ueberwache die Zuendungsspannung.
+  if (digitalRead(relayIGN) == LOW) {
+    delay(30000);
+    // Wenn nach 30 Sek immernoch LOW, dann Spannung von BATT ausschalten
+    if (digitalRead(relayIGN) == LOW) {
+      digitalWrite(relayBATT, HIGH);
+    }
   }
-  strip2.show();
-  for (uint16_t i = 0; i < strip3.numPixels(); i++)
-  {
-      strip3.setPixelColor(i, strip3.Color(150, 210, 255));
-  }
-  strip3.show();
-  for (uint16_t i = 0; i < strip4.numPixels(); i++)
-  {
-      strip4.setPixelColor(i, strip4.Color(150, 210, 255));
-  }
-  strip4.show();
-  for (uint16_t i = 0; i < strip5.numPixels(); i++)
-  {
-      strip5.setPixelColor(i, strip5.Color(150, 210, 255));
-  }
-  strip5.show();
-  for (uint16_t i = 0; i < strip6.numPixels(); i++)
-  {
-      strip6.setPixelColor(i, strip6.Color(150, 210, 255));
-  }
-  strip6.show();
-  // Wiederhole loop() erst nach 30 Sekunden
-  delay(30000);
 }
